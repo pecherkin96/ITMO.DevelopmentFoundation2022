f81152dbb859acef3523d51feceeed9b29dbeece
pecherkin96 <103280072+pecherkin96@users.noreply.github.com>
2022-04-14 10:21:29 +0300

    Add files via upload

--- ITMO.DevelopmentFoundation2022.First_prog/Program.cs
+++ ITMO.DevelopmentFoundation2022.First_prog/Program.cs
@@ -10,16 +10,16 @@ namespace ITMO.DevelopmentFoundation2022.First_prog
     {
         static void Main(string[] args)
         {
-            string answer = "да";
+            string answer = "да"; // Переменная для создания цикла do while
             do
             {
                 int cost_f = 47;
                 Console.WriteLine("Введите сколько вам надо литорв и сколько у вас денег.");
-                int litr = Convert.ToInt32(Console.ReadLine());
-                int money = Convert.ToInt32(Console.ReadLine());
-                int bill = litr * cost_f;
-                int remaider_m = money - bill;
-                int remainder_f = remaider_m / cost_f;
+                int litr = Convert.ToInt32(Console.ReadLine()); // litr – количество литров, которое хотим заправить 
+                int money = Convert.ToInt32(Console.ReadLine()); // money -количество денег, которое есть
+                int bill = litr * cost_f; // bill – стоимость  введённого объёма топлива
+                int remaider_m = money - bill; // remainder_m – остаток денежных средств
+                int remainder_f = remaider_m / cost_f; // remainder_f – количество топлива, которое можно дозаправить на остаток средств
                 if (remaider_m >= 0)
                     Console.WriteLine("За " + litr + " л. топлива вы заплатите " + bill + " рублей.\n" +
                                       "Ваш остаток " + remaider_m + " рублей.\n" +
